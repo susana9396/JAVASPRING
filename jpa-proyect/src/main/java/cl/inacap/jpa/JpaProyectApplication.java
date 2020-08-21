27765062954eb4ecbac5bb358bdead7db84b180f
SL <susanavlr@live.cl>
2020-08-20 20:30:06 -0400

    tarea

---
+++ jpa-proyect/src/main/java/cl/inacap/jpa/JpaProyectApplication.java
@@ -0,0 +1,13 @@
+package cl.inacap.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProyectApplication.class, args);
+	}
+
+}
